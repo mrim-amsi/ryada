bdaf5b368ae72355b47388d79e6c62e35e069576
mariam alamssi <malamassi@students.iugaza.edu.ps>
2023-05-22 15:42:40 +0300

    push

--- ryada/Services/searchService.cs
+++ ryada/Services/searchService.cs
@@ -1,4 +1,7 @@
-﻿namespace ryada.Services
+﻿using Microsoft.EntityFrameworkCore;
+using ryada.Models;
+
+namespace ryada.Services
 {
     public class SearchService
     {
@@ -6,6 +9,13 @@
         public SearchService(AppDBContext dbContext)
         {
             _dbContext = dbContext;
+        }
+        public async Task<List<Book>> SearchAsync(string word)
+        {
+            var expensis = await _dbContext.Books
+               .Where(p => p.Category.Name.Contains(word)).ToListAsync();
+
+            return expensis;
         }
     }
 }
